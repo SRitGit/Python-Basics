0092c162c5c17415ddd376933f89cf4213b87aef
SRitGit <66678661+SRitGit@users.noreply.github.com>
2024-06-20 17:11:03 +0530

    Add files via upload

---
+++ Project one.py
@@ -0,0 +1,9 @@
+signal=str(input("Enter the signal"))
+if (signal=="red"):
+    print("Please Stop")
+elif (signal=="green"):
+    print("Go")
+elif (signal=="yellow"):
+    print("Please slow down")
+else:
+    print("Invalid Color")
